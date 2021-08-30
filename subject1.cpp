f1de37c0bdc3fe33e9a8a0d2872f4ace263dd9b3
huiluo <2491389456@qq.com>
2021-08-30 22:11:03 +0800

    写入文件函数

--- subject1.cpp
+++ subject1.cpp
@@ -8,16 +8,24 @@ using namespace std;
 string Question[Max];
 string answer[Max];
 
-void input()
+int input()
 {
     int i = 0;
     ifstream ifs;
     ifs.open("question.txt",ios::in);
-    while (ifs >> Question[i])
+    if (!ifs.is_open())
     {
-        
+        cout << "文件打开异常" << endl;
+        return i;
     }
     
+    while (ifs >> Question[i])
+    {
+        cout << Question[i] << endl;
+        i++;
+    }
+    // cout << i << endl;
+    return i;
 }
 
 void calculate()
@@ -25,12 +33,21 @@ void calculate()
     
 }
 
-void output()
+void output(int num)
 {
-
+    int i = 0;
+    ofstream ofs;
+    ofs.open("answer.txt",ios::out);
+    for ( i = 0; i < num; i++)
+    {
+        ofs << Question[i] << "=" << answer[i] << endl;
+    }
+    ofs.close();
 }
 
 int main ()
 {
-
+    int num;
+    num = input();
+    output(num);
 }
