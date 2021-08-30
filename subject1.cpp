30fce15fc11bf2eae6dc3ac858b957a56211a96e
huiluo <2491389456@qq.com>
2021-08-30 18:19:46 +0800

    基本框架和读取文件函数

---
+++ subject1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#define Max 100
+
+using namespace std;
+
+string Question[Max];
+string answer[Max];
+
+void input()
+{
+    int i = 0;
+    ifstream ifs;
+    ifs.open("question.txt",ios::in);
+    while (ifs >> Question[i])
+    {
+        
+    }
+    
+}
+
+void calculate()
+{
+    
+}
+
+void output()
+{
+
+}
+
+int main ()
+{
+
+}
