dcd45c4335c07b6a9b116e2a61b1708999f1d8f5
huiluo <2491389456@qq.com>
2021-08-31 01:00:24 +0800

    得到数据进行相应计算

--- subject1.cpp
+++ subject1.cpp
@@ -1,12 +1,14 @@
 #include<iostream>
+#include<string.h>
 #include<string>
 #include<fstream>
+#include<stdlib.h>
 #define Max 100
 
 using namespace std;
 
 string Question[Max];
-string answer[Max];
+int answer[Max];
 
 int input()
 {
@@ -28,8 +30,71 @@ int input()
     return i;
 }
 
-void calculate()
+void calculate(int num)
 {
+    int number1,number2,j,k,l;
+    int K,L;
+    char num1[10],num2[10];
+    char type[10];                            //存放计算类型
+    for (int i = 0; i < num ; i++)
+    {
+        K = 0;
+        L = 0;
+        for ( j = 0; Question[i][j] != '('; j++)
+        {
+            type[j] = Question[i][j];
+        }
+        type[j] = '\0';
+
+        memset(num1,0,sizeof(num1));                          //赋初始值
+        memset(num2,0,sizeof(num2));
+
+        for ( k = j + 1; Question[i][k] != ')' && Question[i][k] != ','; k++)
+        {
+            num1[K] = Question[i][k];
+            K++;
+        }
+        num1[K] = '\0';
+        if (Question[i][k] == ',')
+        {
+            for ( l = k + 1; Question[i][l] != ')'; l++)
+            {
+                num2[L] = Question[i][l];
+                L++;
+            }
+            num2[L] = '\0';
+            
+            number2 = atoi(num2);
+        }
+        
+        //cout << num1 << endl;
+        number1 = atoi(num1);
+        if (strcmp(type,"add") == 0)
+        {
+            answer[i] = number1 + number2;
+        }
+        else if (strcmp(type,"sub") == 0)
+        {
+            answer[i] = number1 - number2;
+        }
+        else if (strcmp(type,"muti") == 0)
+        {
+            answer[i] = number1 * number2;
+        }
+        else if (strcmp(type,"div") == 0)
+        {
+            answer[i] = number1 / number2;
+        }
+        else if (strcmp(type,"doubleMe") == 0)
+        {
+            answer[i] = number1 + number1;
+        }
+        //cout << number1 << " " << number2 << endl;
+    }
+    // for (int i = 0; i < num; i++)
+    // {
+    //     cout << answer[i];
+    // }
     
 }
 
@@ -49,5 +114,6 @@ int main ()
 {
     int num;
     num = input();
+    calculate(num);
     output(num);
 }
